bf4eb07b0eb4e2b747fd150644e78839444462ef
soyjavierlinares <javihp79@gmail.com>
2021-04-26 15:58:20 +0200

    initial structure

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+export function $(selector: string): NodeList {
+  return document.querySelectorAll(selector);
+}
+
+export function HideElement(selector: string): boolean {
+  const element = <HTMLElement>document.querySelector(selector);
+  if (element) {
+    element.style.display = "none";
+    return true;
+  } else {
+    return false;
+  }
+}
